9908d118bba16d5780e28e37072155ab5bad380c
Ahmed-Taw <ahmed_a1442010@outlook.com>
2022-12-26 18:11:10 +0200

    add Update mutation

--- GraphQl/Mutations/ProductMutation.cs
+++ GraphQl/Mutations/ProductMutation.cs
@@ -13,6 +13,9 @@ namespace GraphQlTest.Mutations
             Field<ProductType>("createProduct", 
                 arguments: new QueryArguments(new QueryArgument<ProductInputType> { Name = "product" }),
                 resolve: context => { return productService.AddProduct(context.GetArgument<Product>("product")); });
+            Field<ProductType>("updateProduct",
+                arguments: new QueryArguments(new QueryArgument<IntGraphType> { Name = "id" }, new QueryArgument<ProductInputType> { Name = "product" }),
+                resolve: context => { return productService.UpdateProduct(context.GetArgument<int>("id"), context.GetArgument<Product>("product")); });
         }
     }
 }
